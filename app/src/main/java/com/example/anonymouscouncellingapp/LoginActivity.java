8a2f46baaa264ce4d0b494c7846a52420741fc72
Alfred Khanyile <mr.khanyile21@gmail.com>
2024-04-30 23:23:25 +0000

    connected my android app to the server (mysql server) to check if it works and it does. Now I need to add Items to the database from the registration point

--- app/src/main/java/com/example/anonymouscouncellingapp/LoginActivity.java
+++ app/src/main/java/com/example/anonymouscouncellingapp/LoginActivity.java
@@ -1,23 +1,39 @@
 package com.example.anonymouscouncellingapp;
 
+import static com.example.anonymouscouncellingapp.links.Links.LOGIN_PHP;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 public class LoginActivity extends AppCompatActivity {
-    EditText email, password;
+    private OkHttpClient client;
+    private EditText username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        email = findViewById(R.id.client_username);
+        username = findViewById(R.id.client_username);
         password = findViewById(R.id.client_password);
+        client = new OkHttpClient();
 
     }
 
@@ -27,12 +43,41 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void loginUser(View view) {
-        String email = this.email.getText().toString();
+        String username = this.username.getText().toString();
         String password = this.password.getText().toString();
 
-        if (!email.isEmpty() && !password.isEmpty()){
-            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
-            finish();
+        if (!username.isEmpty() && !password.isEmpty()){
+            RequestBody requestBody = new FormBody.Builder()
+                    .add("username", username)
+                    .add("password", password)
+                    .build();
+
+            Request request = new Request.Builder()
+                    .url(LOGIN_PHP)
+                    .post(requestBody)
+                    .build();
+
+            client.newCall(request).enqueue(new Callback() {
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                    e.printStackTrace();
+                }
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                assert response.body() != null;
+                                Toast.makeText(LoginActivity.this, response.body().string(), Toast.LENGTH_SHORT).show();
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    });
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/example/anonymouscouncellingapp/links/Links.java
+++ app/src/main/java/com/example/anonymouscouncellingapp/links/Links.java
@@ -1,4 +1,5 @@
 package com.example.anonymouscouncellingapp.links;
 
 public class Links {
+    public static String LOGIN_PHP = "https://lamp.ms.wits.ac.za/home/s1858930/login.php";
 }
