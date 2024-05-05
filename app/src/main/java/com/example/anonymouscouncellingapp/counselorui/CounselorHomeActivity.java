fa9df90cb8015307e42fa2a292530ac29e22d522
Alfred Khanyile21 <mr.khanyile21@gmail.com>
2024-05-05 16:14:12 +0000

    Added a UI for things related to client since I believe the ui's will be different. Also added a class to store userDetails that are used on almost all the Activity files

---
+++ app/src/main/java/com/example/anonymouscouncellingapp/counselorui/CounselorHomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.anonymouscouncellingapp.counselorui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.anonymouscouncellingapp.R;
+
+public class CounselorHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_counselor_home);
+    }
+}
---
+++ app/src/main/java/com/example/anonymouscouncellingapp/details/UserDetails.java
@@ -0,0 +1,8 @@
+package com.example.anonymouscouncellingapp.details;
+
+public class UserDetails {
+    public static String user_id;
+    public static String user_type;
+    public static String username;
+
+}
